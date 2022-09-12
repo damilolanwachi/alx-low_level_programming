ee737f59d2cd86598b90df3cf86a26a47b3cca91
damilolanwachi <damilolanwachi@gmail.com>
2022-09-12 05:55:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,15 @@
 
 /**
  *
- *  * Description: main - Prints out a string to stdout.
+ * Description: main - Prints out a string to stdout.
  *
- *   * Return: 0 if success.
+ * Return: 0
  *
  *    */
 
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
