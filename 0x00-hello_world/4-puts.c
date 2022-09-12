c8fecbeedf3567324f6a54c096b2b0f8dac44072
damilolanwachi <damilolanwachi@gmail.com>
2022-09-12 05:34:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
+ * Description: main - Prints out a string to stdout.
+ *this program prints "Programming is like building a multilingual puzzle"
+ * Return: 0 if success.
  *
  *    */
 int main(void)
